417be3df906b8fa6eb37e248de4068607bc1cfcc
Kapil Sharma <kapil.pundit@gmail.com>
2023-11-21 12:59:39 +0530

    Added validations to the received arguments

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ class Item:
     and as we have already defined the default value of quantity as 0 which is int.
     '''
     def __init__(self, name: str, price: float, quantity = 0) -> None:
+        # Run validations to the received arguments
+        assert price >= 0, f"Price {price} is not greater than or equal to 0"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to 0"
+        
+        # Assign object properties to self
         self.name = name
         self.price = price
         self.quantity = quantity
@@ -16,5 +21,5 @@ class Item:
 item1 = Item("Mobile", 300, 2)
 print(item1.calculate_total_price())
 
-item1 = Item("Laptop", 1500, 3)
+item1 = Item("Laptop", 1500, -3)
 print(item1.calculate_total_price())
